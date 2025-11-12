174c698daf707d978014840a1cb0b7c83773e434
lLuizSales <luiz.edu.car33@gmail.com>
2025-11-12 09:15:59 -0300

    .

--- dash.py
+++ dash.py
@@ -6,16 +6,22 @@ st.set_page_config(layout = "wide")
 
 st.title("Dashboard sobre a Escolaridade e Desemprego")
 
-df = pd.read_csv("base-dados2.csv", delimiter = ";", encoding = "utf-8")
-
-df["Brasil e Grande Região"].unique()
+df = pd.read_csv("base-dados.csv", delimiter = ";", encoding = "utf-8")
 
 regiao = st.sidebar.selectbox("Brasil e Regiões", df["Brasil e Grande Região"].unique())
 
 df_filtro = df[df["Brasil e Grande Região"] == regiao]
 
-total = df_filtro.groupby("Nível de instrução")[["Variável - Pessoas de 14 anos ou mais de idade, desocupadas na semana de referência (Mil pessoas)"]].sum().reset_index()
+df_filtro["Ano"] = df_filtro["Trimestre"].str.extract(r"(\d{4})")
 
-fig_total = px.pie(total, values = "Variável - Pessoas de 14 anos ou mais de idade, desocupadas na semana de referência (Mil pessoas)", names = "Nível de instrução")
+df_filtro["Ano"] = df_filtro["Ano"].astype(int)
+
+ano = st.sidebar.selectbox("Ano", df_filtro["Ano"].unique())
+
+df_filtro = df_filtro[df_filtro["Ano"] == ano]
+
+total = df_filtro.groupby("Nível de instrução")[["Pessoas de 14 anos ou mais de idade, desocupadas na semana de referência (Mil pessoas)"]].sum().reset_index()
+
+fig_total = px.pie(total, values = "Pessoas de 14 anos ou mais de idade, desocupadas na semana de referência (Mil pessoas)", names = "Nível de instrução")
 
 st.plotly_chart(fig_total)
