90dbe2e6acbcb9b18925bcc7a4175c53fad5096b
lLuizSales <luiz.edu.car33@gmail.com>
2025-11-12 20:44:14 -0300

    .

--- dash.py
+++ dash.py
@@ -3,27 +3,53 @@ import pandas as pd
 import plotly.express as px
 
 st.set_page_config(layout = "wide")
-
-st.title("Dashboard sobre a Escolaridade e Desemprego")
+st.set_page_config(page_title = "👨‍🎓👷‍♂️ Escolaridade e Desemprego")
+st.title("Análise sobre a influência da escolaridade sobre o desemprego")
 
 df = pd.read_csv("base-dados.csv", delimiter = ";", encoding = "utf-8")
+df["Pessoas de 14 anos ou mais de idade, desocupadas na semana de referência (Mil pessoas)"] = df["Pessoas de 14 anos ou mais de idade, desocupadas na semana de referência (Mil pessoas)"].astype(int)
+df["Ano"] = df["Trimestre"].str.extract(r"(\d{4})")
+df["Ano"] = df["Ano"].astype(int)
+df["Trimestre número"] = df["Trimestre"].str.extract(r"(\d+)")
+df["Trimestre número"] = df["Trimestre número"].astype(int)
 
 regiao = st.sidebar.selectbox("Brasil e Regiões", df["Brasil e Grande Região"].unique())
+df_filtro_r = df[df["Brasil e Grande Região"] == regiao]
 
-df_filtro = df[df["Brasil e Grande Região"] == regiao]
+ano = st.sidebar.selectbox("Ano", df_filtro_r["Ano"].unique())
+df_filtro = df_filtro_r[df_filtro_r["Ano"] == ano]
 
-df_filtro["Ano"] = df_filtro["Trimestre"].str.extract(r"(\d{4})")
-
-df_filtro["Ano"] = df_filtro["Ano"].astype(int)
-
-ano = st.sidebar.selectbox("Ano", df_filtro["Ano"].unique())
-
-df_filtro = df_filtro[df_filtro["Ano"] == ano]
-
-df_filtro["Pessoas de 14 anos ou mais de idade, desocupadas na semana de referência (Mil pessoas)"] = df_filtro["Pessoas de 14 anos ou mais de idade, desocupadas na semana de referência (Mil pessoas)"].astype(int)
+col1,col2 = st.columns(2)
 
 total = df_filtro.groupby("Nível de instrução")[["Pessoas de 14 anos ou mais de idade, desocupadas na semana de referência (Mil pessoas)"]].sum().reset_index()
+fig_total = px.pie(total, 
+                   values = "Pessoas de 14 anos ou mais de idade, desocupadas na semana de referência (Mil pessoas)", 
+                   names = "Nível de instrução",
+                   title = f"Total do Desemprego por Nível de Instrução - {regiao}/{ano}")
+fig_total.update_layout(legend_title = "Nível de instrução")
+col1.plotly_chart(fig_total)
 
-fig_total = px.pie(total, values = "Pessoas de 14 anos ou mais de idade, desocupadas na semana de referência (Mil pessoas)", names = "Nível de instrução")
+df_trimestre = df_filtro.groupby(["Trimestre", "Nível de instrução"])[["Pessoas de 14 anos ou mais de idade, desocupadas na semana de referência (Mil pessoas)"]].sum().reset_index()
+fig_total_anos = px.line(df_trimestre, 
+                        x = "Trimestre",
+                        y = "Pessoas de 14 anos ou mais de idade, desocupadas na semana de referência (Mil pessoas)", 
+                        color = "Nível de instrução",
+                        title = f"Evolução do Desemprego por Nível de Instrução - {regiao}/{ano}")
 
-st.plotly_chart(fig_total)
+fig_total_anos.update_layout(yaxis_title="Mil pessoas")
+
+col2.plotly_chart(fig_total_anos)
+
+df_anos = df_filtro_r.groupby(["Ano", "Trimestre número"])[["Pessoas de 14 anos ou mais de idade, desocupadas na semana de referência (Mil pessoas)"]].sum().reset_index()
+
+fig_anos = px.bar(df_anos,
+                  x = "Ano",
+                  y = "Pessoas de 14 anos ou mais de idade, desocupadas na semana de referência (Mil pessoas)",
+                  color = "Trimestre número",
+                  title = f"Evolução do Desemprego por ano - {regiao}")
+
+fig_anos.update_xaxes(type = "category")
+
+fig_anos.update_layout(yaxis_title="Mil pessoas")
+
+st.plotly_chart(fig_anos)
